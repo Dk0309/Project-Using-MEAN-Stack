5d49fb256e03458c7cbb73f456168a70ce92b58d
Deepak Kumar <70138380+Dk0309@users.noreply.github.com>
2021-02-19 17:39:53 +0530

    Add files via upload

--- Server/routes/index.router.js
+++ Server/routes/index.router.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
 
+const ctrlUser = require('../controllers/user.controller');
+//const ctrlTeacher = require('../controllers/teacher.controller');
+const jwtHelper = require('../config/jwtHelper');
+
+router.post('/register', ctrlUser.register);
+router.post('/authenticate', ctrlUser.authenticate);
+router.get('/students/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+//router.post('/authentication', ctrlTeacher.authentication);
+//router.post('/manageteacher', ctrlTeacher.manageteacher);
+ //router.get('/myProfile',jwtHelper.verifyJwtToken, ctrlTeacher.myProfile);
+
+module.exports = router;
